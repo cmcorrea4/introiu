4160e1b87fcc3841e65837fda3f6a5369081eb55
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-03-11 21:52:41 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+from PIL import Image
+
+st.title(" Mi Primera App!!")
+
+st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales.")
+st.write("Facilmente puedo realizar backend y frontend.")
+image = Image.open('Interfaces Mult2.png')
