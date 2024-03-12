0c79f94c8d1ffd99f51f8f33a874ca3e229f7666
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-03-11 21:55:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ st.title(" Mi Primera App!!")
 
 st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales.")
 st.write("Facilmente puedo realizar backend y frontend.")
-image = Image.open('Interfaces Mult2.png')
+image = Image.open('imagenci.jpg')
