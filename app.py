e8e69dab70a691dcdc0b244c4aa93801be64b98c
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-03-11 22:10:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.title(" Aplicaciones para Ciudades Inteligentes")
 
 st.header("En este espacio podrás obtener información de tu ciudad.")
 
-image = Image.open('imagenci.jpg')
+image = Image.open('Sistemaiot.png')
 st.image(image, caption='Inteligencia Urbana')
 st.write("Enlace para mi sistema de internet de las cosas")
 st.write("Ingresa al link [link](https://demo.thingsboard.io/dashboards/9a61e170-565c-11ee-b6bf-f9525dc44ab3)")
